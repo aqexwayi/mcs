a760ea0df2e82aef6b58faa9a5cba985ac224c86
aqexwayi <aqexwayi@163.com>
2013-09-11 08:41:00 +0800

    0.6.3
    change version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mcs "0.6.2"
+(defproject mcs "0.6.3"
   :description "micro-block control system"
   :url "http://micro-block-control-system.com/"
   :dependencies [[org.clojure/clojure "1.5.1"]
